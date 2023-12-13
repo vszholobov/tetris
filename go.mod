ee143a2fc16f436fa54ed312a788cf4fd0608c8a
vszholobov <vszholobov@gmail.com>
2023-12-14 01:19:47 +0300

    Process player input from keyboard

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.21
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mattn/go-tty v0.0.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/mattn/go-tty"
+	"log"
 	"math/rand"
 	"sync"
 	"tetris/field"
@@ -9,48 +11,89 @@ import (
 
 func main() {
 	extField := field.MakeField()
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(gameField *field.Field) {
+
+	keyboardInputChannel := make(chan rune)
+	// input
+	go func(keyboardInputChannel chan<- rune) {
+		keyPressedChannel, err := tty.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer keyPressedChannel.Close()
+
+		for {
+			r, err := keyPressedChannel.ReadRune()
+			if err != nil {
+				log.Fatal(err)
+			}
+			//fmt.Println("Key press => " + string(r))
+			keyboardInputChannel <- r
+		}
+	}(keyboardInputChannel)
+	// game
+	go func(gameField *field.Field, keyboardInputChannel chan rune) {
 		piece := SelectNextPiece(gameField)
 		for {
-			field.PrintField(gameField)
-			moveType := rand.Intn(4)
-			switch moveType {
-			case 0:
-				piece.MoveLeft()
-			case 1:
-				piece.MoveRight()
-			case 2:
-				piece.MoveDown()
-			case 3:
-				piece.Rotate(field.Left)
-			}
+			inputControl(keyboardInputChannel, gameField)
+
 			if !piece.MoveDown() {
 				gameField.Val.Or(gameField.Val, piece.GetVal())
 				piece = SelectNextPiece(gameField)
 			}
 			gameField.Clean()
-
-			time.Sleep(time.Second / 10)
 		}
-	}(&extField)
+	}(&extField, keyboardInputChannel)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	wg.Wait()
+}
+
+func inputControl(
+	keyboardInputChannel chan rune,
+	gameField *field.Field,
+) {
+	timeout := time.After(time.Second / 8)
+	for {
+		field.PrintField(gameField)
+		select {
+		case moveType := <-keyboardInputChannel:
+			switch moveType {
+			case 100:
+				// d
+				gameField.CurrentPiece.MoveLeft()
+			case 97:
+				// a
+				gameField.CurrentPiece.MoveRight()
+			case 115:
+				// s
+				gameField.CurrentPiece.MoveDown()
+			case 101:
+				// e
+				gameField.CurrentPiece.Rotate(field.Left)
+			case 113:
+				// q
+				gameField.CurrentPiece.Rotate(field.Right)
+			}
+		case <-timeout:
+			return
+		}
+	}
 }
 
 func SelectNextPiece(gameField *field.Field) field.Piece {
 	pieceTypeRnd := rand.Intn(5)
+	// TODO: square type
 	var pieceType field.PieceType
 	if pieceTypeRnd == 0 {
-		pieceType = field.TShape
-	} else if pieceTypeRnd == 1 {
-		pieceType = field.ZigZagLeft
-	} else if pieceTypeRnd == 2 {
-		pieceType = field.ZigZagRight
-	} else if pieceTypeRnd == 3 {
 		pieceType = field.IShape
-	} else if pieceTypeRnd == 4 {
+	} else if pieceTypeRnd == 1 {
 		pieceType = field.LShape
+	} else if pieceTypeRnd == 2 {
+		pieceType = field.TShape
+	} else if pieceTypeRnd == 3 {
+		pieceType = field.ZigZagRight
+	} else if pieceTypeRnd == 4 {
+		pieceType = field.ZigZagLeft
 	}
 	piece := field.MakePiece(gameField, pieceType)
 	gameField.CurrentPiece = &piece
